cd23715f1c313c32ba2da1c2b5069cc23d63f1ff
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-17 03:00:02 +0800

    initial vesion done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { logoASDBuddy } from "../assets";
 const Navbar = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [isNavOpen, setNavOpen] = useState(false);
@@ -18,22 +18,23 @@ const Navbar = () => {
   const toggleNav = () => setNavOpen(!isNavOpen);
   const toggleDarkMode = () => setDarkMode(!isDarkMode);
 
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+
+    console.log("clicked");
+
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <nav className="border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900">
-      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
-            Flowbite
-          </span>
-        </a>
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-center justify-between p-4">
+        <img src={logoASDBuddy} className="h-12" />
         <button
           onClick={toggleNav}
-          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
           aria-controls="navbar-dropdown"
           aria-expanded={isNavOpen}
         >
@@ -60,11 +61,11 @@ const Navbar = () => {
           } w-full md:block md:w-auto`}
           id="navbar-dropdown"
         >
-          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
+          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900 rtl:space-x-reverse">
             <li>
               <a
                 href="#"
-                className="block rounded bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 dark:bg-blue-600 md:dark:bg-transparent md:dark:text-blue-500"
+                className="block rounded bg-blue-700 px-3 py-2 text-white dark:bg-blue-600 md:bg-transparent md:p-0 md:text-blue-700 md:dark:bg-transparent md:dark:text-blue-500"
                 aria-current="page"
               >
                 Home
@@ -73,7 +74,7 @@ const Navbar = () => {
             <li className="relative">
               <button
                 onClick={toggleDropdown}
-                className="flex w-full items-center justify-between rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                className="flex w-full items-center justify-between rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:text-white md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
               >
                 Dropdown{" "}
                 <svg
@@ -137,15 +138,15 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
               >
                 Services
               </a>
             </li>
             <li>
               <a
-                href="#"
-                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                onClick={() => scrollToSection("pricing")}
+                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
               >
                 Pricing
               </a>
@@ -153,50 +154,13 @@ const Navbar = () => {
             <li>
               <a
                 href="#"
-                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                className="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
               >
                 Contact
               </a>
             </li>
           </ul>
         </div>
-        <button
-          onClick={toggleDarkMode}
-          className="ml-3 h-10 w-10 rounded-full bg-gray-200 p-1.5 text-gray-800 dark:bg-gray-700 dark:text-gray-200"
-          aria-label="Toggle dark mode"
-        >
-          {isDarkMode ? (
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3v1m0 16v1m8.66-6.66-.7.7m-13.2-.7-.7.7m0-10.6.7-.7m13.2.7.7-.7M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3a9 9 0 100 18 9 9 0 000-18z"
-              />
-            </svg>
-          )}
-        </button>
       </div>
     </nav>
   );
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,6 +5,7 @@ const PRICINGDETAILS = [
     class: "Free",
     pricing: "INR 0",
     description: "(Always)",
+    buttonDescription: "Begin Free",
     items: [
       "Free app Download",
       "Access to Blogs",
@@ -12,36 +13,62 @@ const PRICINGDETAILS = [
       "Access 1 week of content on each FREEDOM Course Once a year 6-week parent training access",
     ],
   },
+  {
+    class: "Guided Self Study",
+    pricing: "INR 385/week",
+    description: "(Single payment - 13 weeks course)",
+    buttonDescription: "Begin a Freedom",
+    items: [
+      "Free app Download",
+      "Access to Blogs",
+      "Access to Community",
+      "Access ONE 13 week full FREEDOM Course",
+      "Parent Training and Q&A every 15 days - LIVE Group",
+      "Even when your FREEDOM course is over, you will be invited to CoffeeTime with Founder",
+    ],
+  },
+  {
+    class: "Customized",
+    pricing: "INR 5000/Week",
+    description: "(Single payment - 12 weeks)",
+    buttonDescription: "Begin Customized",
+    items: [
+      "Free app Download",
+      "Access to Blogs",
+      "Access to Community",
+      "12 weeks - Goal-Based - NDBI Guided Program",
+      "Everyday parent videos ASDBUDDY feedback",
+      "Individualised Family Based - Weekly -60minute consultation with hands-on Parent Training and Demos",
+      "CoffeeTime with Founder - Life-long access",
+    ],
+  },
 ];
 
 const PricingCard = ({ item }) => {
   return (
-    <div className="rounded-lg border p-6 shadow-lg">
+    <div className="max-w-72 rounded-lg border p-6 shadow-sm shadow-buddyBlueLight transition-all hover:shadow-md hover:shadow-buddyBlue">
       <h3 className="text-xl font-semibold text-gray-900">{item.class}</h3>
       <p className="mt-4 text-gray-500">{item.pricing}</p>
       <p className="text-sm text-gray-400">{item.description}</p>
-      <button className="mt-4 w-full rounded-md bg-indigo-600 px-4 py-2 font-semibold text-white">
-        Buy plan
-      </button>
+
       <ul className="mt-6 space-y-4 text-gray-600">
         {item.items.map((description, i) => (
           <li className="flex">
             ✔️ <div className="pl-2">{description}</div>{" "}
           </li>
         ))}
-        <li>✔️ 5 products</li>
-        <li>✔️ Up to 1,000 subscribers</li>
-        <li>✔️ Basic analytics</li>
-        <li>✔️ 48-hour support response time</li>
       </ul>
+      <button className="mt-4 w-full rounded-md bg-indigo-600 px-4 py-2 font-semibold text-white">
+        {item.buttonDescription}
+      </button>
     </div>
   );
 };
 const Pricing = () => {
   return (
-    <div className="container mx-auto py-16">
+    <div id="pricing" className="container mx-auto mt-48 py-16">
       <div className="text-center">
-        <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+        <h2 className="text-3xl font-thin text-gray-900 sm:text-6xl">
           Providing you value and paying our employees!
         </h2>
         <p className="mt-4 text-lg text-gray-500">
@@ -49,65 +76,11 @@ const Pricing = () => {
           then commit
         </p>
       </div>
-      <div className="mx-auto mt-12 grid max-w-lg gap-6 lg:max-w-none lg:grid-cols-3">
-        {PRICINGDETAILS.map((item, i) => (
-          <PricingCard key={i} item={item} />
-        ))}
-        {/* Basic Plan */}
-        <div className="rounded-lg border p-6 shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-900">Basic</h3>
-          <p className="mt-4 text-gray-500">$15/month</p>
-          <p className="text-sm text-gray-400">
-            $12 per month if paid annually
-          </p>
-          <button className="mt-4 w-full rounded-md bg-indigo-600 px-4 py-2 font-semibold text-white">
-            Buy plan
-          </button>
-          <ul className="mt-6 space-y-4 text-gray-600">
-            <li>✔️ 5 products</li>
-            <li>✔️ Up to 1,000 subscribers</li>
-            <li>✔️ Basic analytics</li>
-            <li>✔️ 48-hour support response time</li>
-          </ul>
-        </div>
-        {/* Essential Plan */}
-        <div className="rounded-lg border p-6 shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-900">Essential</h3>
-          <p className="mt-4 text-gray-500">$30/month</p>
-          <p className="text-sm text-gray-400">
-            $24 per month if paid annually
-          </p>
-          <button className="mt-4 w-full rounded-md bg-indigo-600 px-4 py-2 font-semibold text-white">
-            Buy plan
-          </button>
-          <ul className="mt-6 space-y-4 text-gray-600">
-            <li>✔️ Everything in Basic</li>
-            <li>✔️ 25 products</li>
-            <li>✔️ Up to 10,000 subscribers</li>
-            <li>✔️ Advanced analytics</li>
-            <li>✔️ 24-hour support response time</li>
-            <li>✔️ Marketing automations</li>
-          </ul>
-        </div>
-        {/* Growth Plan */}
-        <div className="rounded-lg border p-6 shadow-lg">
-          <h3 className="text-xl font-semibold text-gray-900">Growth</h3>
-          <p className="mt-4 text-gray-500">$60/month</p>
-          <p className="text-sm text-gray-400">
-            $48 per month if paid annually
-          </p>
-          <button className="mt-4 w-full rounded-md bg-indigo-600 px-4 py-2 font-semibold text-white">
-            Buy plan
-          </button>
-          <ul className="mt-6 space-y-4 text-gray-600">
-            <li>✔️ Everything in Essential</li>
-            <li>✔️ Unlimited products</li>
-            <li>✔️ Unlimited subscribers</li>
-            <li>✔️ Advanced analytics</li>
-            <li>✔️ 1-hour dedicated support response time</li>
-            <li>✔️ Marketing automations</li>
-            <li>✔️ Custom reporting tools</li>
-          </ul>
+      <div className="flex items-center justify-center p-10">
+        <div className="grid max-w-4xl gap-12 md:grid-cols-3">
+          {PRICINGDETAILS.map((item, i) => (
+            <PricingCard key={i} item={item} />
+          ))}
         </div>
       </div>
     </div>
