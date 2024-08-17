66ecd26dba5a8cb3bb6a922dac4bf157b0ae17b2
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-18 02:31:45 +0800

    tabbed freedom

--- src/App.jsx
+++ src/App.jsx
@@ -23,18 +23,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" index element={<Home />} />
-        <Route path="freedom" element={<Freedom />}>
-          <Route path="1" element={<Freedom1 />} />
-          <Route path="2" element={<Freedom2 />} />
-          <Route path="3" element={<Freedom3 />} />
-          <Route path="4" element={<Freedom4 />} />
-          <Route path="5" element={<Freedom5 />} />
-          <Route path="6" element={<Freedom6 />} />
-          <Route path="7" element={<Freedom7 />} />
-          <Route path="8" element={<Freedom8 />} />
-          <Route path="9" element={<Freedom9 />} />
-          <Route path="10" element={<Freedom10 />} />
-        </Route>
+        <Route path="freedom" element={<Freedom />}></Route>
       </Routes>
     </Router>
   );
--- src/Pages/freedom/Freedom.jsx
+++ src/Pages/freedom/Freedom.jsx
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 const CARDS = [
-  { path: "1", heading: "Freedom 1", description: "Encouraging Looking" },
-  { path: "2", heading: "Freedom 2", description: "Simple Expectation" },
-  { path: "3", heading: "Freedom 3", description: "Engagement Established" },
-  { path: "4", heading: "Freedom 4", description: "Dynamism Initiated" },
-  { path: "3", heading: "Freedom 3", description: "Engagement Established" },
-  { path: "3", heading: "Freedom 3", description: "Engagement Established" },
-  { path: "3", heading: "Freedom 3", description: "Engagement Established" },
-  { path: "3", heading: "Freedom 3", description: "Engagement Established" },
-  { path: "3", heading: "Freedom 3", description: "Engagement Established" },
+  {
+    id: 1,
+    path: "1",
+    heading: "Freedom 1",
+    description: "Encouraging Looking",
+  },
+  { id: 2, path: "2", heading: "Freedom 2", description: "Simple Expectation" },
+  {
+    id: 3,
+    path: "3",
+    heading: "Freedom 3",
+    description: "Engagement Established",
+  },
+  { id: 4, path: "4", heading: "Freedom 4", description: "Dynamism Initiated" },
+  {
+    id: 5,
+    path: "5",
+    heading: "Freedom 5",
+    description: "Engagement Established",
+  },
+  {
+    id: 6,
+    path: "6",
+    heading: "Freedom 6",
+    description: "Engagement Established",
+  },
+  {
+    id: 7,
+    path: "7",
+    heading: "Freedom 7",
+    description: "Engagement Established",
+  },
+  {
+    id: 8,
+    path: "8",
+    heading: "Freedom 8",
+    description: "Engagement Established",
+  },
+  {
+    id: 9,
+    path: "9",
+    heading: "Freedom 9",
+    description: "Engagement Established",
+  },
+  {
+    id: 10,
+    path: "10",
+    heading: "Freedom 10",
+    description: "Engagement Established",
+  },
 ];
 
 const FreeCard = ({ card }) => {
@@ -30,17 +71,48 @@ const FreeCard = ({ card }) => {
     </div>
   );
 };
+
+const FreedomTabs = () => {
+  const [activeTab, setActiveTab] = useState(1);
+
+  return (
+    <div className="md:flex">
+      <ul className="mb-4 flex space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400 md:mb-0 md:mr-4 md:flex-col">
+        {CARDS.map((freedom) => (
+          <li key={freedom.id}>
+            <button
+              onClick={() => setActiveTab(freedom.id)}
+              className={`inline-flex w-32 items-center rounded-lg px-4 py-3 ${
+                activeTab === freedom.id
+                  ? "bg-blue-700 text-white dark:bg-blue-600"
+                  : "bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700"
+              }`}
+            >
+              <span>{freedom.heading}</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="text-medium w-full rounded-lg bg-gray-50 p-6 text-gray-500 dark:bg-gray-800 dark:text-gray-400">
+        {CARDS.map((freedom) =>
+          activeTab === freedom.path ? (
+            <div key={freedom.id}>
+              <h3 className="mb-2 text-lg font-bold text-gray-900 dark:text-white">
+                {freedom.name}
+              </h3>
+              <p>{freedom.content}</p>
+            </div>
+          ) : null,
+        )}
+      </div>
+    </div>
+  );
+};
+
 const Freedom = () => {
   return (
-    <div className="container mx-auto">
-      <h1>Freedom Main Section</h1>
-      <div className="grid items-start justify-start gap-12 md:grid-cols-4">
-        {CARDS.map((card, i) => (
-          <FreeCard key={i} card={card} />
-        ))}
-      </div>
-      <Outlet />{" "}
-      {/* This is where the nested route content (like Freedom1) will be rendered */}
+    <div className="container mx-auto p-2">
+      <FreedomTabs />
     </div>
   );
 };
