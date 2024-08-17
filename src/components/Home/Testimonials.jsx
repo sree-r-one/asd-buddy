ece187139dd761f60a655ab5fef9ff228dc1a649
sree-r-one <sreeraj_ec@hotmail.com>
2024-08-17 23:46:56 +0800

    Testimonials added

--- src/components/Home/Testimonials.jsx
+++ src/components/Home/Testimonials.jsx
@@ -1,5 +1,56 @@
 import React, { useState, useEffect } from "react";
 
+const TESTIMONIALS = [
+  {
+    photo:
+      "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+    alias: "Pri - Mother to 3 yr old – Mumbai, June 2016",
+    data: "ASDBUDDY provides a holistic approach to a child's development, allowing parents to see their child as a whole rather than through isolated therapies. The program encourages collaboration and thoughtful goal-setting, helping parents gain clarity and confidence in addressing their child's needs. Video feedback and regular sessions offer valuable insights, enabling parents to better understand and support their child's social and behavioral progress.",
+  },
+  {
+    photo:
+      "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+    alias: "Mother of Adi, Oct 2023 - Freedom 3, Dubai",
+    data: "ASDBuddy provided the breakthrough my son needed when traditional therapies stopped working. It helped me shift my parenting approach, bringing renewed hope and a structured way to address the core issues of Autism. The platform's videos, explanations, and regular feedback sessions have been invaluable, offering practical guidance and supporting meaningful progress in my son's development.",
+  },
+  {
+    photo:
+      "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80",
+    alias: "Pri - Mother to 6 yr old – TamilNadu, June 2020",
+    data: "I have done other therapies for my child before starting the relationship model, but have not seen much progress with the child. The child is made to do a task like a robot and complete it without the child not even being aware as to what they are doing. The child is only physically present in that environment. But here they make sure that the child happily engages with us in our day-to-day activities and is also given in the home environment.",
+  },
+];
+
+const Testimony = ({ testimony }) => {
+  return (
+    <div className="flex flex-col rounded-xl bg-white shadow-sm shadow-buddyBlueLight transition-all hover:shadow-buddyBlue hover:drop-shadow-lg dark:bg-neutral-900">
+      <div className="flex-auto p-4 md:p-6">
+        <p className="text-base italic text-gray-800 dark:text-neutral-200 md:text-lg">
+          {testimony.data}
+        </p>
+      </div>
+
+      <div className="rounded-b-xl bg-gray-100 p-4 dark:bg-neutral-800 md:px-7">
+        <div className="flex items-center gap-x-3">
+          <div className="shrink-0">
+            <img
+              className="size-8 rounded-full sm:h-[2.875rem] sm:w-[2.875rem]"
+              src={testimony.photo}
+              alt="Avatar"
+            />
+          </div>
+
+          <div className="grow">
+            <p className="text-sm font-semibold text-gray-800 dark:text-neutral-200 sm:text-base">
+              {testimony.alias}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Testimonials = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
@@ -18,123 +69,22 @@ const Testimonials = () => {
   return (
     <div className="overflow-hidden">
       <div className="relative mx-auto max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14">
-        {/* Dark Mode Toggle Button */}
-        <div className="mb-6 flex justify-end"></div>
-
         {/* Title */}
         <div className="mb-6 w-3/4 max-w-2xl sm:mb-10 md:mb-16 lg:w-1/2">
-          <h2 className="text-2xl font-semibold text-black sm:text-3xl lg:text-4xl">
-            Loved by business and individuals across the globe
+          <h2 className="text-4xl font-semibold text-black sm:text-3xl lg:text-6xl">
+            Testimonials
           </h2>
+          <p className="mt-2 font-thin leading-6">
+            Your identity is strictly kept confidential
+          </p>
         </div>
         {/* End Title */}
 
         {/* Grid */}
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {/* Card */}
-          <div className="flex h-auto">
-            <div className="flex flex-col rounded-xl bg-white dark:bg-neutral-900">
-              <div className="flex-auto p-4 md:p-6">
-                <p className="text-base italic text-gray-800 md:text-lg dark:text-neutral-200">
-                  " With Preline, we're able to easily track our performance in
-                  full detail. It's become an essential tool for us to grow and
-                  engage with our audience. "
-                </p>
-              </div>
-
-              <div className="rounded-b-xl bg-gray-100 p-4 md:px-7 dark:bg-neutral-800">
-                <div className="flex items-center gap-x-3">
-                  <div className="shrink-0">
-                    <img
-                      className="size-8 rounded-full sm:h-[2.875rem] sm:w-[2.875rem]"
-                      src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-                      alt="Avatar"
-                    />
-                  </div>
-
-                  <div className="grow">
-                    <p className="text-sm font-semibold text-gray-800 sm:text-base dark:text-neutral-200">
-                      Josh Tyson
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-neutral-400">
-                      Product Manager | Capsule
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* End Card */}
-
-          {/* Card */}
-          <div className="flex h-auto">
-            <div className="flex flex-col rounded-xl bg-white dark:bg-neutral-900">
-              <div className="flex-auto p-4 md:p-6">
-                <p className="text-base italic text-gray-800 md:text-lg dark:text-neutral-200">
-                  " In September, I will be using this theme for 2 years. I went
-                  through multiple updates and changes and I'm very glad to see
-                  the consistency and effort made by the team. "
-                </p>
-              </div>
-
-              <div className="rounded-b-xl bg-gray-100 p-4 md:px-7 dark:bg-neutral-800">
-                <div className="flex items-center gap-x-3">
-                  <div className="shrink-0">
-                    <img
-                      className="size-8 rounded-full sm:h-[2.875rem] sm:w-[2.875rem]"
-                      src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
-                      alt="Avatar"
-                    />
-                  </div>
-
-                  <div className="grow">
-                    <p className="text-sm font-semibold text-gray-800 sm:text-base dark:text-neutral-200">
-                      Luisa
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-neutral-400">
-                      Senior Director of Operations | Fitbit
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* End Card */}
-
-          {/* Card */}
-          <div className="flex h-auto">
-            <div className="flex flex-col rounded-xl bg-white dark:bg-neutral-900">
-              <div className="flex-auto p-4 md:p-6">
-                <p className="text-base italic text-gray-800 md:text-lg dark:text-neutral-200">
-                  " Refreshing and Thought provoking design and it changes my
-                  view about how I design the websites. Great typography, modern
-                  clean white design, nice tones of the color. "
-                </p>
-              </div>
-
-              <div className="rounded-b-xl bg-gray-100 p-4 md:px-7 dark:bg-neutral-800">
-                <div className="flex items-center gap-x-3">
-                  <div className="shrink-0">
-                    <img
-                      className="size-8 rounded-full sm:h-[2.875rem] sm:w-[2.875rem]"
-                      src="https://images.unsplash.com/photo-1579017331263-ef82f0bbc748?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=900&h=900&q=80"
-                      alt="Avatar"
-                    />
-                  </div>
-
-                  <div className="grow">
-                    <p className="text-sm font-semibold text-gray-800 sm:text-base dark:text-neutral-200">
-                      Alisa Williams
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-neutral-400">
-                      Entrepreneur | Happy customer
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* End Card */}
+        <div className="grid gap-8 sm:grid-cols-3 md:gap-12">
+          {TESTIMONIALS.map((testimony, i) => (
+            <Testimony key={i} testimony={testimony} />
+          ))}
         </div>
         {/* End Grid */}
 
